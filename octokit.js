0acf9e69ffc1afbf27e3d8a9b90f3fc3d6b02e77
alwaysmaria1 <41010002+alwaysmaria1@users.noreply.github.com>
2025-03-26 13:03:54 -0400

    Create octokit.js

---
+++ octokit.js
@@ -0,0 +1,12 @@
+const diffText = response.data;
+    console.log(diffText);
+
+    await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+      owner,
+      repo,
+      issue_number: pull_number,
+      body: messageForNewPRs,
+      headers: {
+        'x-github-api-version': '2022-11-28',
+      },
+    });
